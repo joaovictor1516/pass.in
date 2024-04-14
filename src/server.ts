230e2a3e565c8c990cda55ed772ec7b03f4e79d7
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-04-14 13:54:33 -0300

    criacao da rota post juntamente com a coneccao da aplicacao com o banco, juntamente com a validacao das respostas recebidas

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import fastify from "fastify";
+import { z } from "zod";
+import { PrismaClient } from "@prisma/client";
 
 const app = fastify();
+const prisma = new PrismaClient({
+    log:["query"]
+});
 
 //API -> Rest
 //Protocolo HTTP -> É o protocolo responsável pela semantica do tipo da operacao feita na rota da api 
@@ -11,10 +16,25 @@ const app = fastify();
 //parametros de rotas (routs params) => identificação de recursos  http://localhost:3333/users/5 (obrigatorio)
 //cabeçalhos (headers) => contexto da requisição
 
+app.post("/events", async (request, reply) => {
+    const creatEventSchema = z.object({
+        tittle: z.string().min(4),
+        details: z.string().nullable(),
+        maximumAttendees: z.number().int().positive().nullable()
+    });
 
+    const data = creatEventSchema.parse(request.body); //verifica se os dados recebidos sao validos pelos campos de validacao criados no creatEventSchema.
 
-app.get("/", () => {
-    return "Ola mundo";
+    const event = await prisma.event.create({
+         data:{
+            tittle: data.tittle,
+            details: data.details,
+            maximumAttendees: data.maximumAttendees,
+            slug: new Date().toISOString()
+         }
+    });
+
+    return reply.status(201).send({eventId: event.id});
 });
 
 app.listen({port: 3333}).then(() => {
