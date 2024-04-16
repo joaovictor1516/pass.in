f3b2b2fdddcd654229bfba7bdae532da974d50db
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-04-16 11:57:16 -0300

    adicao da rota de criacao do evento ao servidor

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { creatEvent } from "./routs/createEvents";
 import fastify from "fastify";
 
 const app = fastify();
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
+
+app.register(creatEvent);
 
 app.listen({port: 3333}).then(() => {
     console.log("HTTP server running");
