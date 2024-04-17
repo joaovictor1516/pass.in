22fd4e221ff35569da7c364fcc5ee7e55ea98b04
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-04-17 15:59:36 -0300

    refatoracao do codigo

--- src/routs/getAttendeeBadge.ts
+++ src/routs/getAttendeeBadge.ts
@@ -14,10 +14,8 @@ export async function getAttendeeBadge(app: FastifyInstance){
                 response: {
                     200: z.object({
                         attendee: z.object({
-                            id: z.number().int(),
                             name: z.string(),
-                            email: z.string().email(),
-                            eventId: z.string().uuid()
+                            email: z.string().email()
                         })
                     })
                 }
@@ -27,10 +25,8 @@ export async function getAttendeeBadge(app: FastifyInstance){
 
             const attendee = await prisma.attendees.findUnique({
                 select: {
-                    id: true,
                     name: true,
                     email: true,
-                    eventId: true
                 },
                 where: {
                     id: attendeeId
@@ -43,10 +39,8 @@ export async function getAttendeeBadge(app: FastifyInstance){
 
             return reply.code(200).send({
                 attendee: {
-                    id: attendee.id,
                     name: attendee.name,
-                    email: attendee.email,
-                    eventId: attendee.eventId
+                    email: attendee.email
                 }
             });
         });
