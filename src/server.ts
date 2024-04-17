6e984e662407f74d34922f599e5b7def3e6aa2fa
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-04-17 12:54:33 -0300

    adicao da rota getEvent no servidor

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@ import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod
 import { registerForEvent } from "./routs/registerForEvent";
 import { creatEvent } from "./routs/createEvents";
 import fastify from "fastify";
+import { getEvent } from "./routs/getEvent";
 
 const app = fastify();
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
+app.register(getEvent);
 app.register(creatEvent);
 app.register(registerForEvent);
 
