6ab2ad22758736a1ca4959bc1c274a7ff120927f
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-04-19 00:02:26 -0300

    criacao da rota checkIn. a sua responsabilidade e a autenticacao do usuario na entrada do evento e verificar se o ingresso ja foi utilizado.

---
+++ src/routs/checkIn.ts
@@ -0,0 +1,42 @@
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+
+export async function checkIn(app: FastifyInstance){
+    app
+        .withTypeProvider<ZodTypeProvider>()
+        .get("attendee/:attendeeId/check-in", {
+            schema: {
+                params: z.object({
+                    attendeeId: z.coerce.number().int()
+                }),
+                response:{
+                    201: z.object({
+                        attendeeId: z.number().int()
+                    })
+                }
+            }
+        }, async (request, reply) => {
+            const {attendeeId} = request.params;
+
+           const checkIn = await prisma.checkIn.findUnique({
+            where: {
+                attendeesId: attendeeId
+            }
+           });
+
+           if(checkIn !== undefined){
+                reply.code(404);
+                throw new Error("Attendee already checked in.");
+           };
+
+           await prisma.checkIn.create({
+                data: {
+                    attendeesId: attendeeId
+                }
+           });
+
+           return reply.code(201).send({attendeeId});
+        });
+};
