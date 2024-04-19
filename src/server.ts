c51a2abe32b0e8e82d17bbcc64b77b9d7dae6dc3
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-04-19 00:01:56 -0300

    adicao da rota checkIn

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,14 @@ import { creatEvent } from "./routs/createEvents";
 import fastify from "fastify";
 import { getEvent } from "./routs/getEvent";
 import { getAttendeeBadge } from "./routs/getAttendeeBadge";
+import { checkIn } from "./routs/checkIn";
 
 const app = fastify();
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
+app.register(checkIn);
 app.register(getEvent);
 app.register(creatEvent);
 app.register(getAttendeeBadge);
