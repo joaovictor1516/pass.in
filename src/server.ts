e1806978ef00c930e1909ae0f2fd2b0aadc4563c
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-04-22 23:13:23 -0300

    adicao da rota getEventAttendees.

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,10 @@ import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod
 import { registerForEvent } from "./routs/registerForEvent";
 import { creatEvent } from "./routs/createEvents";
 import fastify from "fastify";
-import { getEvent } from "./routs/getEvent";
+
+import { getEventAttendees } from "./routs/getEventAttendees";
 import { getAttendeeBadge } from "./routs/getAttendeeBadge";
+import { getEvent } from "./routs/getEvent";
 import { checkIn } from "./routs/checkIn";
 
 const app = fastify();
@@ -16,6 +18,7 @@ app.register(getEvent);
 app.register(creatEvent);
 app.register(getAttendeeBadge);
 app.register(registerForEvent);
+app.register(getEventAttendees);
 
 app.listen({port: 3333}).then(() => {
     console.log("HTTP server running");
