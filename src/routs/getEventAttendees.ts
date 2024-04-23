9d44572d443831766a32ed4b1b980c0affee0a47
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-04-23 16:49:44 -0300

    adicao da paginacao. ela e feita usando funcoes proprias do fastify e do zod.

--- src/routs/getEventAttendees.ts
+++ src/routs/getEventAttendees.ts
@@ -1,7 +1,7 @@
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { FastifyInstance } from "fastify";
 import { prisma } from "../lib/prisma";
-import { z } from "zod";
+import { number, z } from "zod";
 
 export async function getEventAttendees(app: FastifyInstance){
     app
@@ -11,18 +11,22 @@ export async function getEventAttendees(app: FastifyInstance){
                 params: z.object({
                     eventId: z.string().uuid()
                 }),
+                querystring: z.object({
+                    pageIndex: z.string().nullable().default("0").transform(Number)
+                }),
                 response: {}
             }
         }, async (request, reply) => {
             const { eventId } = request.params;
+            const { pageIndex } = request.query;
 
             const attendees = await prisma.attendees.findMany({
                 where: {
                     eventId
-                }
-            })
-
-        
+                },
+                take: 10,
+                skip: pageIndex * 10
+            });
 
             return reply.status(200).send(attendees);
         });
