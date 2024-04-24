2f6e8d288ca3e2e6e7120f9fc97f8952d9ca48bc
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-04-23 21:22:33 -0300

    criacao da paginacao e selecao dos dados que devem ser enviados. a importancia da paginacao e o carregamento dinamico pelo fato dos dados serem recebidos de acordo da pagina, da rota, que o usuario esta acessando.

--- src/routs/getEventAttendees.ts
+++ src/routs/getEventAttendees.ts
@@ -21,6 +21,17 @@ export async function getEventAttendees(app: FastifyInstance){
             const { pageIndex } = request.query;
 
             const attendees = await prisma.attendees.findMany({
+                select: {
+                    id: true,
+                    name: true,
+                    email: true,
+                    createdAt: true,
+                    checkIn: {
+                        select: {
+                            createdAt: true
+                        }
+                    }
+                },
                 where: {
                     eventId
                 },
@@ -28,6 +39,18 @@ export async function getEventAttendees(app: FastifyInstance){
                 skip: pageIndex * 10
             });
 
-            return reply.status(200).send(attendees);
+            return reply.status(200).send({ 
+                attendees: attendees.map((attendees) => {
+                    const object = {
+                        id: attendees.id,
+                        name: attendees.name,
+                        email: attendees.email,
+                        createdAt: attendees.createdAt,
+                        checkInAt: attendees.checkIn ? attendees.checkIn.createdAt : null
+                    };
+
+                    return object;
+                })
+            });
         });
 };
