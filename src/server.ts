7f169c8d635295401bb4f72a3b498b397a73ca19
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-04-30 13:44:05 -0300

    adicao e configuracao da documentacao da api.

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
-import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { serializerCompiler, validatorCompiler, jsonSchemaTransform } from "fastify-type-provider-zod";
 import { registerForEvent } from "./routs/registerForEvent";
 import { creatEvent } from "./routs/createEvents";
+import fastifySwaggerUi from "@fastify/swagger-ui";
+import fastifySwagger from "@fastify/swagger";
 import fastify from "fastify";
 
 import { getEventAttendees } from "./routs/getEventAttendees";
@@ -10,12 +12,28 @@ import { checkIn } from "./routs/checkIn";
 
 const app = fastify();
 
+app.register(fastifySwagger, {
+    swagger: {
+        consumes: ["application/json"],
+        produces: ["aplication/json"],
+        info: {
+            title: "pass.in",
+            description: "API da aplicação pass.in criada durante a NLW Unit.",
+            version: "1.0.0"
+        }
+    },
+    transform: jsonSchemaTransform
+});
+
+app.register(fastifySwaggerUi, {
+    routePrefix: "/docs"
+});
+
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(checkIn);
 app.register(getEvent);
-app.register(creatEvent);
 app.register(getAttendeeBadge);
 app.register(registerForEvent);
 app.register(getEventAttendees);
